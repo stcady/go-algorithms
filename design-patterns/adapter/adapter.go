8bf5ce098fd50e83bfe108203657969e11ec8da8
Steven Cady <scady@apple.com>
2022-03-12 19:32:23 -0800

    adapters

---
+++ design-patterns/adapter/adapter.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+type IProcess interface {
+	process()
+}
+
+type Adaptee struct {
+	adapterType int
+}
+
+type Adapter struct {
+	adaptee Adaptee
+}
+
+func (adapter Adapter) process() {
+	fmt.Println("Adapter process")
+	adapter.adaptee.convert()
+}
+
+func (adaptee Adaptee) convert() {
+	fmt.Println("Adapteee convert method")
+}
+
+func main() {
+	var processor IProcess = Adapter{}
+	processor.process()
+}
